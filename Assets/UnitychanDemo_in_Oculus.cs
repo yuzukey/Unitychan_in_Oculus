ec9b39406ce95cf240111521bde2f2e59ebe1a8b
yuzukey <syana000@gmail.com>
2015-06-18 16:48:34 +0900

    changed that when push Enter, Unitychan gestures

---
+++ Assets/UnitychanDemo_in_Oculus.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitychanDemo_in_Oculus : MonoBehaviour {
+
+    private Animator animator;
+    public bool isSpace = true;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKey("space")){
+            animator.SetBool("gesture", true);
+            isSpace = true;
+        }else{
+            animator.SetBool("gesture", false);
+            isSpace = false;
+        }
+	}
+}
